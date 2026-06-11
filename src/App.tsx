8bfc3535501a329b134460846f6ee3391f30a94f
KENHIGHER <64080642+KENHIGHER@users.noreply.github.com>
2026-06-11 19:02:04 +0800

    Fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Result from "@/pages/Result";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/play" element={<Play />} />
